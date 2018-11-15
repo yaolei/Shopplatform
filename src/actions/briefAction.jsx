cde87d480f07d7fb388f3ce22e071b785d7c5f9a
Yao Lei <yao_lei_java@hotmail.com>
2018-11-15 10:46:54 +0800

    11

--- src/actions/briefAction.jsx
+++
@@ -1,8 +0,0 @@
-export const BRIEF_INFO = "BRIEF_INFO";
-
-export function setBriefInfo(info) {
-    return {
-        type: BRIEF_INFO,
-        data: info
-    };
-}
--- src/middlewares/mainMiddleWare.jsx
+++
@@ -1,7 +0,0 @@
-export default function(store) {
-    return function(next) {
-        return function(action) {
-            return next(action);
-        }
-    }
-} 
